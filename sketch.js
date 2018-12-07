57534b39fcd7d7e7719ddad5f388b6faf6af871c
Eryl Kenner <eryl.kenner@gmail.com>
2018-12-07 00:25:44 -0800

    Refactored code and abstacted code into functions.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,4 @@
-/*global
-    p5, createCanvas, frameRate, width, height, createVector, translate, rotate, mouseX, mouseY, line, stroke, strokeWeight, noStroke, ellipse, background, fill,
-    text, rect, sq, dist,
-	
-	Vehicle
-*/
-"use strict";
+
 var vehicles = [],
 	food = [],
 	poison = [];
@@ -20,16 +14,15 @@ var NUM_VEHICLES = 100,
 function setup(){
 	frameRate(20);
 	createCanvas(1200,600);
-	var i;
-	for(i=0; i<NUM_VEHICLES; i++){
+	for(let i=0; i<NUM_VEHICLES; i++){
 		vehicles[i] = new Vehicle(random(width), random(height), null);
 	}
 	
-	for(var i=0; i<NUM_FOOD; i++){
+	for(let i=0; i<NUM_FOOD; i++){
 		food.push(createVector(random(width), random(height)));
 	}
 	
-	for(i=0; i<NUM_POISON; i++){
+	for(let i=0; i<NUM_POISON; i++){
 		poison.push(createVector(random(width), random(height)));
 	}
     
@@ -41,17 +34,8 @@ function mouseClicked(){
 }
 
 function draw(){
-    if(vehicles[0] == undefined){
-        clear();
-        background(51);
-        textSize(50);
-        fill(255);
-        text("Simulation Over!", 50, 100);
-        text("Framecount: " + frameCount, 50, 200);
-        text("maxChildren: " + maxChildren, 50, 300);
-		text("maxHealth: " + maxHealth, 50, 400);
-        noStroke();
-        noLoop();
+    if(vehicles.length == 0){
+        showEndScreen();
     } else{
         background(51);
 
@@ -65,38 +49,65 @@ function draw(){
             vehicles.push(new Vehicle(random(width), random(height), null));
         }
 
-        var i;
-        //Draw food
-        for(i=0; i<food.length; i++){
-            fill(0, 255, 0);
-            noStroke();
-            ellipse(food[i].x, food[i].y, 8, 8);
-        }
-        //Draw poison
-        for(i=0; i<poison.length; i++){
-            fill(255, 0, 0);
-            noStroke();
-            ellipse(poison[i].x, poison[i].y, 8, 8);
-        }
+        drawFood();
+        drawPoison();
+        updateVehicles();
+    }
+}
 
-        for(i=vehicles.length-1; i>=0; i--){
-            vehicles[i].boundaries();
-            vehicles[i].behaviors(food, poison);
-            vehicles[i].update();
-            vehicles[i].display();
-            
-            var isDead = vehicles[i].dead();
-            if((vehicles[i].lifetime >= Math.round(250 / vehicles[i].health)) && !isDead){
-            //if(random() < (0.001 * vehicles[i].health)){
-                var newVehicle = vehicles[i].clone();
-                vehicles.push(newVehicle);
-            }
-            
-            if(isDead){
-                food.push(createVector(vehicles[i].position.x, vehicles[i].position.y));
-                vehicles.splice(i, 1);
-            }
-            
+function showEndScreen(){
+    clear();
+    background(51);
+    textSize(50);
+    fill(255);
+    text("Simulation Over!", 50, 100);
+    text("Framecount: " + frameCount, 50, 200);
+    text("maxChildren: " + maxChildren, 50, 300);
+	text("maxHealth: " + maxHealth, 50, 400);
+    noStroke();
+    noLoop();
+}
+
+function drawFood(){
+    for(i=0; i<food.length; i++){
+        fill(0, 255, 0);
+        noStroke();
+        ellipse(food[i].x, food[i].y, 8, 8);
+    }
+}
+
+function drawPoison(){
+    for(i=0; i<poison.length; i++){
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(poison[i].x, poison[i].y, 8, 8);
+    }
+}
+
+function tryToReproduce(i){
+    if(vehicles[i].lifetime >= Math.round(250 / vehicles[i].health)){
+        let newVehicle = vehicles[i].clone();
+        vehicles.push(newVehicle);
+    }
+}
+
+function dropFood(i){
+    food.push(createVector(vehicles[i].position.x, vehicles[i].position.y));
+    vehicles.splice(i, 1);
+}
+
+function updateVehicles(){
+    for(let i=vehicles.length-1; i>=0; i--){
+        vehicles[i].boundaries();
+        vehicles[i].behaviors(food, poison);
+        vehicles[i].update();
+        vehicles[i].display();
+        if(vehicles[i].dead()){
+            dropFood(i);
+        } else{
+            tryToReproduce(i);
         }
     }
 }
+
+
